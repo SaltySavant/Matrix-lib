862114a65b0db6e4ecbf1ff5a9abf12d39cb080e
SaltySavant <vasya2005kill1199@yandex.com>
2023-02-14 22:47:38 +0600

    Full refactoring v0.1

--- lib/Matrix.h
+++ lib/Matrix.h
@@ -5,30 +5,36 @@
 #include <typeinfo>
 #include <limits>
 #include <utility>
+#include <cstdint>
 
 template<typename T>
 class matrix{
 private:
     static_assert(std::is_arithmetic<T>::value, "T must be an arithmetic type");
-    std::vector<std::vector<T>> vmatrix;                                          ///Matrix
-    std::vector<std::string> split(const std::string& s, char delimiter);         ///Splits a string into a substring by value///
+    std::vector<T> vmatrix;
+    size_t row;
+    size_t column;
+    std::uint8_t threads = 2;
+    void setStringToMatrix(const std::string&);
+    bool add(size_t index,const T& data);                  ///Add data in a matrix///
+    bool change(size_t index,const T& data);               ///Change data in a matrix///
 public:
     /////Constructors-Destructor/////
     matrix() = default;
     ~matrix() = default;
-    matrix(int,int);                              ///Constructor to create an empty matrix///
-    explicit matrix(std::vector<std::vector<T>>); ///Constructor to copy from vector///
+    matrix(size_t,size_t);                              ///Constructor to create an empty matrix///
     //////////////Methods to use//////////////
+    void setNumberOfThreads(const std::uint8_t);  ///Changes the number of threads for calculations///
     void display();                               ///Print matrix to console///
-    void loadMatrix(const std::string&);                ///Reading a matrix from a file///
-    void setMatrix(const std::string& mMatrix);   ///Methods to create a matrix from string///
-    bool add(int , int , T);                      ///Add data in a matrix///
-    bool change(int , int , T );                  ///Change data in a matrix///
+    void loadMatrix(const std::string&);          ///Reading a matrix from a file///
+    void setMatrix (const std::string&);          ///Methods to create a matrix from string///
+    bool add(size_t ,size_t , const T&);          ///Add data in a matrix///
+    bool change(size_t , size_t , const T&);      ///Change data in a matrix///
     bool clear();                                 ///Clear data and size a matrix///
     const std::pair<size_t,size_t> size() const;  ///Size matrix///
-    void resize(int n ,int m);                    ///Resize matrix///
+    void resize(size_t n ,size_t m);              ///Resize matrix///
     void resize(const std::pair<size_t,size_t>&); ///Resize matrix with help pair///
-    const std::vector<std::vector<T>> get();      ///Returns the matrix///
+    const T getElement(size_t r , size_t c) const;
     //////////Overloaded Operators//////////
     matrix<T> operator+ (const matrix<T>& other) const ;    ///Matrix addition///
     matrix<T> operator- (const matrix<T>& other) const ;    ///Matrix difference///
@@ -36,56 +42,80 @@ public:
     matrix<T> operator* (const matrix<T>& other) const;     ///Matrix multiplication///
 };
 
-
 ///////////////Implementation///////////////
 #include <iostream>
 #include <sstream>
+#include <stdexcept>
+#include <thread>
 
 //////////////Private///////////
 ///Splits a string into a substring by value///
 template <typename T>
-std::vector<std::string> matrix<T>::split(const std::string& s, char delimiter) {
-    std::vector<std::string> tokens;
-    std::string token;
-    std::istringstream token_stream(s);
-    while (std::getline(token_stream, token, delimiter)) {
-        tokens.push_back(token);
+void matrix<T>::setStringToMatrix(const std::string& s) {
+
+    vmatrix.clear();
+    vmatrix.shrink_to_fit();
+    std::istringstream is(s);
+    T num;
+    while (is >> num) {
+        vmatrix.push_back(num);
     }
-    return tokens;
+    if(is.fail()){
+        is.clear();
+    }
+
+    if(vmatrix.size() < row * column){
+        vmatrix.resize(row * column , 0);
+    }
 }
 
+///Add or change data in a matrix///
+template <typename T>
+bool matrix<T>::add(size_t index,const T& data){
+    try {
+        vmatrix.at(index) += data;
+    } catch (const std::out_of_range& e) {
+        std::cout << "Out of range: '" << e.what() <<"' \nYou should expand the size of the matrix with the 'resize' function and try adding the data again" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+template <typename T>
+bool matrix<T>::change(size_t index,const T& data){
+    try {
+        vmatrix.at(index) = data;
+    } catch (const std::out_of_range& e) {
+        std::cout << "Out of range: '" << e.what() <<"' \nYou should expand the size of the matrix with the 'resize' function and try adding the data again" << std::endl;
+        return false;
+    }
+    return true;
+}
 //////////////Public////////////////
-///Constructor to copy vector from matrix///
+///Changes the number of threads for calculations///
 template<typename T>
-matrix<T>::matrix(std::vector<std::vector<T>> mMatrix):vmatrix(mMatrix) {}
+void matrix<T>::setNumberOfThreads(const std::uint8_t numThreads) {
+    if(numThreads <= 0){
+        throw std::invalid_argument("The number of threads used for calculation can't be less than one");
+    }
+    else{
+        threads = numThreads;
+    }
+}
 
 ///Constructor to create an empty matrix///
 template<typename T>
-matrix<T>::matrix(int n, int m) {
-    std::vector<std::vector<int>> vec;
-    int x = 0;
-    for(int i = 0; i < n; i++){
-        std::vector<int> s;
-        for(int k = 0; k < m; k++){
-            s.push_back(x);
-        }
-        vec.push_back(s);
-    }
-    vmatrix = vec;
+matrix<T>::matrix(size_t row, size_t column):row(row),column(column) {
+    vmatrix.resize(row*column);
 }
 
 //////////////Methods to use//////////////
-///Returns the matrix///
-template<typename T>
-const std::vector<std::vector<T>> matrix<T>::get(){
-
-}
 ///Print matrix to console///
 template<typename T>
 void matrix<T>::display() {
-    for(auto const& vec : vmatrix){
-        for(auto const& itr : vec){
-            std::cout << itr << ' ';
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < column; ++j) {
+            std::cout << vmatrix[i * column + j] << " ";
         }
         std::cout << std::endl;
     }
@@ -101,7 +131,7 @@ void matrix<T>::loadMatrix(const std::string& filename) {
         std::string line;
         while (std::getline(mMatrix, line))
         {
-            content += line + '\n';
+            content += line;
         }
     }
     else
@@ -112,45 +142,31 @@ void matrix<T>::loadMatrix(const std::string& filename) {
     }
 
     mMatrix.close();
-    setMatrix(content);
+    setStringToMatrix(content);
+}
+
+
+template<typename T>
+const T matrix<T>::getElement(size_t r , size_t c) const{
+    try {
+        return vmatrix.at(r*column+c);
+    } catch (const std::out_of_range& e) {
+        std::cout << "Out of range: '" << e.what() <<"' \nYou should expand the size of the matrix with the 'resize' function and try getting the data again" << std::endl;
+        return false;
+    }
 }
 
 ///Сreate a vector from string///
 template<typename T>
 void matrix<T>::setMatrix(const std::string &mMatrix) {
-    auto vectors = split(mMatrix,'\n');
-    size_t last;
-    for(auto const& itr:vectors){
-        auto row = matrix::split(itr,' ');
-        std::vector<T> vec;
-        if(row.size() != last && vectors[0] != itr){
-            std::cout << "The matrix must have equal row sizes" << std::endl;
-            continue;
-        }
-
-            for(int i = 0;i < row.size();i++){
-
-                T result;
-             try {
-                    result = (T)std::stod(row[i]);
-             } catch (const std::invalid_argument& e) {
-                 return;
-              }
-
-              vec.push_back(result);
-            }
-
-        last = row.size();
-        vmatrix.push_back(vec);
-
-    }
+    setStringToMatrix(mMatrix);
 }
 
-///Add or change data in a matrix///
+///Add data in a matrix///
 template<typename T>
-bool matrix<T>::add(int n, int m, T data) {
+bool matrix<T>::add(size_t ro, size_t col, const T& data) {
     try {
-        vmatrix.at(n).at(m) += data;
+        vmatrix.at(ro*column+col) += data;
     } catch (const std::out_of_range& e) {
         std::cout << "Out of range: '" << e.what() <<"' \nYou should expand the size of the matrix with the 'resize' function and try adding the data again" << std::endl;
         return false;
@@ -158,17 +174,19 @@ bool matrix<T>::add(int n, int m, T data) {
     return true;
 }
 
-///Add or change data in a matrix///
+
+///Change data in a matrix///
 template<typename T>
-bool matrix<T>::change(int n, int m, T data) {
+bool matrix<T>::change(size_t ro, size_t col,const T& data) {
     try {
-        vmatrix.at(n).at(m) = data;
+        vmatrix.at(ro*column+col) = data;
     } catch (const std::out_of_range& e) {
         std::cout << "Out of range: '" << e.what() <<"' \nYou should expand the size of the matrix with the 'resize' function and try adding the data again" << std::endl;
         return false;
     }
     return true;
 }
+
 
 ///Clear data and size a matrix///
 template<typename T>
@@ -186,25 +204,16 @@ bool matrix<T>::clear() {
 ///Size matrix///
 template<typename T>
 const std::pair<size_t,size_t> matrix<T>::size() const {
-    std::pair<size_t,size_t> size;
-    size.first = vmatrix.size();
-    if(size.first == 0){
-        size.second = 0;
-    }
-    else {
-        size.second = vmatrix[0].size();
-    }
-    return size;
+    return std::pair<size_t,size_t>(row,column);
 }
 
 ///Resize matrix///
 template<typename T>
-void matrix<T>::resize(int n, int m) {
+void matrix<T>::resize(size_t ro, size_t col) {
     try {
-        vmatrix.resize(n);
-        for(int i = 0 ; i < vmatrix.size(); i++){
-            vmatrix[i].resize(m);
-        }
+        row =ro;
+        column = col;
+        vmatrix.resize(ro*col);
     } catch (const std::exception& e) {
         std::cerr << "Resize - an error occurred: " << e.what() << std::endl;
         return;
@@ -216,10 +225,9 @@ void matrix<T>::resize(int n, int m) {
 template<typename T>
 void matrix<T>::resize(const std::pair<size_t,size_t>& size) {
     try {
-        vmatrix.resize(size.first);
-        for(int i = 0 ; i < vmatrix.size(); i++){
-            vmatrix[i].resize(size.second);
-        }
+            row = size.first;
+            column = size.second;
+            vmatrix.resize(row*column);
     } catch (const std::exception& e) {
         std::cerr << "Resize - an error occurred: " << e.what() << std::endl;
         return;
@@ -231,82 +239,76 @@ void matrix<T>::resize(const std::pair<size_t,size_t>& size) {
 ///Matrix addition///
 template<typename T>
 matrix<T> matrix<T>::operator+(const matrix<T> &other) const {
-    if(other.size() != this->size()){
-        return  matrix<T>();
+    if(other.row != this->row || other.column != this->column){
+        throw std::invalid_argument("In order to add or subtract matrices, it is necessary that the number of rows and columns coincide for both matrices");
     }
-    else{
-        matrix<T> newMatrix;
-        size_t n = this->size().first;
-        size_t m = this->size().second;
-        newMatrix.resize(this->size());
-
-        for(auto i = 0;i < n; i++){
-            for(auto j = 0;j < m; j++){
-                newMatrix.change(i,j, this->vmatrix[i][j]+other.vmatrix[i][j]);
+        matrix<T> mxt(this->row , this->column);
+            for (size_t i = 0; i < this->row*this->column; ++i) {
+                mxt.change(i, other.vmatrix[i]+ this->vmatrix[i]);
             }
-        }
-        return newMatrix;
-    }
-
+    return mxt;
 }
+
 
 ///Matrix difference///
 template<typename T>
 matrix<T> matrix<T>::operator-(const matrix<T> &other) const {
-    if(other.size() != this->size()){
-        return  matrix<T>();
+    if(other.row != row || other.column != column){
+        throw std::invalid_argument("In order to add or subtract matrices, it is necessary that the number of rows and columns coincide for both matrices");
     }
-    else{
-        matrix<T> newMatrix;
-        size_t n = this->size().first;
-        size_t m = this->size().second;
-        newMatrix.resize(this->size());
-
-        for(auto i = 0;i < n; i++){
-            for(auto j = 0;j < m; j++){
-                newMatrix.change(i,j, this->vmatrix[i][j]-other.vmatrix[i][j]);
-            }
-        }
-        return newMatrix;
+    matrix<T> mxt(row , column);
+    std::vector<std::thread> usingThreads;
+    usingThreads.reserve(threads);
+    for (size_t i = 0; i < row*column; ++i) {
+        mxt.change(i, vmatrix[i] - other.vmatrix[i]);
     }
-
+    return mxt;
 }
+
 
 ///Multiplication matrix by num///
 template<typename T>
 matrix<T> matrix<T>::operator*(T val) const{
-    matrix<T> newMatrix;
-    size_t n = this->size().first;
-    size_t m = this->size().second;
-    newMatrix.resize(this->size());
-
-    for(auto i = 0;i < n; i++){
-        for(auto j = 0;j < m; j++){
-            newMatrix.change(i,j, this->vmatrix[i][j]*val);
+    matrix<T> mxt(row , column);
+    std::vector<std::thread> usingThreads;
+    usingThreads.reserve(threads);
+    for(std::uint8_t i = 0; i < threads; i++) {
+        usingThreads.emplace_back([&,i](){
+                for (size_t j = 0; j < this->row * this->column; j += threads) {
+                    mxt.change(j, vmatrix[j] * val);
+                }
+            });
         }
+    for(std::thread &t : usingThreads){
+        t.join();
     }
-    return newMatrix;
+    return mxt;
 }
+
 
 ///Matrix multiplication///
 template<typename T>
 matrix<T> matrix<T>::operator*(const matrix<T> &other) const{
-    auto sizeMy = this->size();
-    auto sizeOther = other.size();
-    if(sizeMy.second != sizeOther.first)
-        {
-            return matrix<T>();
-        }
-    
-        matrix<T> newMtx;
-        newMtx.resize(other.size().second, sizeMy.first);
-        
-        for(int i = 0 ; i < sizeMy.first; i++){
-            for(int j = 0 ; j < sizeOther.second ; j++ ){
-                for(int k = 0 ; k < sizeMy.second; k++){
-                    newMtx.add(i, j, this->vmatrix[i][k]*other.vmatrix[k][j]);
+    if(column != other.row){
+        throw std::invalid_argument("Error: Incompatible matrix sizes for multiplication.");
+    }
+    matrix<T> mxt(row, other.column);
+    std::vector<std::thread> usingThreads;
+    usingThreads.reserve(threads);
+
+    for(std::uint8_t i = 0; i < threads; i++){
+        usingThreads.emplace_back([&,i](){
+            for(size_t j = i; j < other.column; j += threads){
+                for(size_t l = 0; l < row; l++){
+                    for(size_t k = 0; k < column; k++){
+                        mxt.vmatrix[l*mxt.column +j] += vmatrix[l*column +k] * other.vmatrix[k* other.column +j];
+                    }
                 }
             }
-        }
-        return newMtx;
+        });
+    }
+   for(std::thread &t : usingThreads){
+        t.join();
+    }
+    return mxt;
 }
