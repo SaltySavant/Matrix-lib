e0bc6ee1b83cd711ffbe185ca79c0a9b7ce3094a
SaltySavant <vasya2005kill1199@yandex.com>
2023-03-03 04:22:08 +0600

    refactoring v0.1

--- lib/Matrix.h
+++ lib/Matrix.h
@@ -6,25 +6,41 @@
 #include <limits>
 #include <utility>
 #include <cstdint>
+#include <cstdlib>
+#include <algorithm>
+#include <chrono>
+#include <memory_resource>
+#include <cstdlib>
+#include <thread>
+#include <mutex>
+
 
 template<typename T>
 class matrix{
 private:
     static_assert(std::is_arithmetic<T>::value, "T must be an arithmetic type");
     std::vector<T> vmatrix;
-    size_t row;
-    size_t column;
-    std::uint8_t threads = 2;
+    std::size_t row;
+    std::size_t column;
+    std::size_t blockSize = 1;
+    std::size_t threads = 1;
     void setStringToMatrix(const std::string&);
     bool add(size_t index,const T& data);                  ///Add data in a matrix///
     bool change(size_t index,const T& data);               ///Change data in a matrix///
+    const std::vector<T>& getMatrix() const;
+    void blocked_column_multi_output_parallel_mmul(const std::vector<T>& B,std::vector<T>& C, std::size_t P,std::size_t start_col,std::size_t end_col,std::size_t chunkSize) const;
+
 public:
     /////Constructors-Destructor/////
     matrix() = default;
     ~matrix() = default;
     matrix(size_t,size_t);                              ///Constructor to create an empty matrix///
+    matrix(const std::vector<T> &vec, const size_t &row, const size_t &column);
     //////////////Methods to use//////////////
-    void setNumberOfThreads(const std::uint8_t);  ///Changes the number of threads for calculations///
+    void setNumberOfThreads(const std::size_t);  ///Changes the number of threads for calculations///
+    void setBlockSize(const std::size_t );       ///Changes the number Blocks for calculations///
+   const std::size_t getNumberOfThreads();
+    const std::size_t getBlockSize();
     void display();                               ///Print matrix to console///
     void loadMatrix(const std::string&);          ///Reading a matrix from a file///
     void setMatrix (const std::string&);          ///Methods to create a matrix from string///
@@ -35,20 +51,80 @@ public:
     void resize(size_t n ,size_t m);              ///Resize matrix///
     void resize(const std::pair<size_t,size_t>&); ///Resize matrix with help pair///
     const T getElement(size_t r , size_t c) const;
+    void setOptimalParams();
     //////////Overloaded Operators//////////
     matrix<T> operator+ (const matrix<T>& other) const ;    ///Matrix addition///
     matrix<T> operator- (const matrix<T>& other) const ;    ///Matrix difference///
     matrix<T> operator* (T val) const ;                     ///Multiplication matrix by num///
     matrix<T> operator* (const matrix<T>& other) const;     ///Matrix multiplication///
 };
-
 ///////////////Implementation///////////////
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
-#include <thread>
+#include <math.h>
 
-//////////////Private///////////
+//////////////Private//////////////
+template<typename T>
+const std::size_t matrix<T>::getNumberOfThreads(){
+    return threads;
+}
+
+template<typename T>
+const std::size_t matrix<T>::getBlockSize(){
+    return blockSize;
+}
+
+template<typename T>
+void matrix<T>::setOptimalParams() {
+    std::size_t numCores = std::thread::hardware_concurrency();
+    std::size_t chunkSize = sqrt(row / (2 * sizeof(size_t)));
+    chunkSize = std::max(chunkSize, static_cast<std::size_t>(1));
+    chunkSize = std::min(chunkSize, row);
+
+    std::size_t num_threads = std::min(numCores, row / chunkSize/ chunkSize);
+    std::max(num_threads, static_cast<std::size_t>(1));
+
+    threads = num_threads;
+    blockSize = chunkSize;
+}
+
+template<typename T>
+void matrix<T>::blocked_column_multi_output_parallel_mmul(const std::vector<T>& B,std::vector<T>& C, std::size_t P,std::size_t start_col,std::size_t end_col,std::size_t chunkSize) const{
+    // For each chunk of columns
+    for (std::size_t col_chunk = start_col; col_chunk < end_col; col_chunk += chunkSize)
+        // For each chunk of rows
+        for (std::size_t row_chunk = 0; row_chunk < row; row_chunk += chunkSize)
+            // For each block of elements in this row of this column chunk
+            // Solve for 16 elements at a time
+            for (std::size_t tile = 0; tile < column; tile += chunkSize)
+                // For apply that tile to each row of the row chunk
+                for (std::size_t r = 0; r < chunkSize; r++)
+                    // For each row in the tile
+                    for (std::size_t tile_row = 0; tile_row < chunkSize; tile_row++)
+                        // Solve for each element in this tile row
+                        for (std::size_t idx = 0; idx < chunkSize; idx++)
+                            C[(r + row_chunk) * P + col_chunk + idx] +=
+                                    vmatrix[(r + row_chunk) * column + tile + tile_row] *
+                                    B[tile * P + tile_row * P + col_chunk + idx];
+}
+
+template<typename T>
+matrix<T>::matrix(const std::vector<T> &vec, const size_t &row, const size_t &column):column(column),row(row) {
+    if(row <= 0 || column <= 0){
+        throw std::invalid_argument("It is not possible to create a matrix with parameters row = 0 , column = 0 (or less)");
+    }
+    else if(vec.size() != row*column){
+        throw std::invalid_argument( "The size of the std::vector<T>& matrix must be equal to the product of row by column");
+    }
+    vmatrix = vec;
+}
+
+template<typename T>
+const std::vector<T>& matrix<T>::getMatrix() const{
+    return vmatrix;
+}
+
 ///Splits a string into a substring by value///
 template <typename T>
 void matrix<T>::setStringToMatrix(const std::string& s) {
@@ -92,9 +168,18 @@ bool matrix<T>::change(size_t index,const T& data){
     return true;
 }
 //////////////Public////////////////
+///Changes the number Blocks for calculations///
+template<typename T>
+void matrix<T>::setBlockSize(const std::size_t blocks) {
+    if(blocks > row || blocks > column){
+        throw std::invalid_argument("The number of blocks used for calculation can't be more than row or column");
+    }
+    blockSize = blocks;
+}
+
 ///Changes the number of threads for calculations///
 template<typename T>
-void matrix<T>::setNumberOfThreads(const std::uint8_t numThreads) {
+void matrix<T>::setNumberOfThreads(const std::size_t numThreads) {
     if(numThreads <= 0){
         throw std::invalid_argument("The number of threads used for calculation can't be less than one");
     }
@@ -106,6 +191,9 @@ void matrix<T>::setNumberOfThreads(const std::uint8_t numThreads) {
 ///Constructor to create an empty matrix///
 template<typename T>
 matrix<T>::matrix(size_t row, size_t column):row(row),column(column) {
+    if(row <= 0 || column <= 0){
+        throw std::invalid_argument("It is not possible to create a matrix with parameters row = 0 , column = 0 (or less)");
+    }
     vmatrix.resize(row*column);
 }
 
@@ -242,10 +330,34 @@ matrix<T> matrix<T>::operator+(const matrix<T> &other) const {
     if(other.row != this->row || other.column != this->column){
         throw std::invalid_argument("In order to add or subtract matrices, it is necessary that the number of rows and columns coincide for both matrices");
     }
-        matrix<T> mxt(this->row , this->column);
-            for (size_t i = 0; i < this->row*this->column; ++i) {
-                mxt.change(i, other.vmatrix[i]+ this->vmatrix[i]);
+    matrix<T> mxt(row , column);
+    std::vector<std::thread> thrds;
+    thrds.reserve(threads);
+    std::size_t n_cols = row*column / threads;
+
+    // Launch threads
+    std::size_t start_col = 0;
+
+    for (std::size_t i = 0; i < threads; i++) {
+        auto end_col = (start_col+n_cols);
+        if(threads-1 == i){
+            auto remains = row*column % threads;
+            if(remains != 0){
+                end_col += remains;
             }
+        }
+        thrds.emplace_back([&,start_col,end_col] {
+            for(std::size_t i = start_col ; i < end_col;i++){
+                mxt.change(i, vmatrix[i] + other.vmatrix[i]);
+            }
+        });
+
+        start_col = end_col;
+    }
+
+    // Wait for all threads to complete
+    for (auto &t : thrds) t.join();
+    thrds.clear();
     return mxt;
 }
 
@@ -257,11 +369,33 @@ matrix<T> matrix<T>::operator-(const matrix<T> &other) const {
         throw std::invalid_argument("In order to add or subtract matrices, it is necessary that the number of rows and columns coincide for both matrices");
     }
     matrix<T> mxt(row , column);
-    std::vector<std::thread> usingThreads;
-    usingThreads.reserve(threads);
-    for (size_t i = 0; i < row*column; ++i) {
-        mxt.change(i, vmatrix[i] - other.vmatrix[i]);
+    std::vector<std::thread> thrds;
+    thrds.reserve(threads);
+    std::size_t n_cols = row*column / threads;
+
+    // Launch threads
+    std::size_t start_col = 0;
+
+    for (std::size_t i = 0; i < threads; i++) {
+        auto end_col = (start_col+n_cols);
+        if(threads-1 == i){
+            auto remains = row*column % threads;
+            if(remains != 0){
+                end_col += remains;
+            }
+        }
+        thrds.emplace_back([&,start_col,end_col] {
+            for(std::size_t i = start_col ; i < end_col;i++){
+                mxt.change(i, vmatrix[i] - other.vmatrix[i]);
+            }
+        });
+
+        start_col = end_col;
     }
+
+    // Wait for all threads to complete
+    for (auto &t : thrds) t.join();
+    thrds.clear();
     return mxt;
 }
 
@@ -270,45 +404,80 @@ matrix<T> matrix<T>::operator-(const matrix<T> &other) const {
 template<typename T>
 matrix<T> matrix<T>::operator*(T val) const{
     matrix<T> mxt(row , column);
-    std::vector<std::thread> usingThreads;
-    usingThreads.reserve(threads);
-    for(std::uint8_t i = 0; i < threads; i++) {
-        usingThreads.emplace_back([&,i](){
-                for (size_t j = 0; j < this->row * this->column; j += threads) {
-                    mxt.change(j, vmatrix[j] * val);
-                }
-            });
+    std::vector<std::thread> thrds;
+    thrds.reserve(threads);
+    std::size_t n_cols = row*column / threads;
+
+    // Launch threads
+    std::size_t start_col = 0;
+
+    for (std::size_t i = 0; i < threads; i++) {
+        auto end_col = (start_col+n_cols);
+        if(threads-1 == i){
+            auto remains = row*column % threads;
+            if(remains != 0){
+                end_col += remains;
+            }
         }
-    for(std::thread &t : usingThreads){
-        t.join();
+        thrds.emplace_back([&,start_col,end_col] {
+            for(std::size_t i = start_col ; i < end_col;i++){
+                mxt.change(i, vmatrix[i] * val);
+            }
+        });
+
+        start_col = end_col;
     }
+
+    // Wait for all threads to complete
+    for (auto &t : thrds) t.join();
+    thrds.clear();
     return mxt;
 }
 
-
 ///Matrix multiplication///
 template<typename T>
-matrix<T> matrix<T>::operator*(const matrix<T> &other) const{
-    if(column != other.row){
+matrix<T> matrix<T>::operator*(const matrix<T> &other) const {
+    if (column != other.row) {
         throw std::invalid_argument("Error: Incompatible matrix sizes for multiplication.");
     }
-    matrix<T> mxt(row, other.column);
-    std::vector<std::thread> usingThreads;
-    usingThreads.reserve(threads);
 
-    for(std::uint8_t i = 0; i < threads; i++){
-        usingThreads.emplace_back([&,i](){
-            for(size_t j = i; j < other.column; j += threads){
-                for(size_t l = 0; l < row; l++){
-                    for(size_t k = 0; k < column; k++){
-                        mxt.vmatrix[l*mxt.column +j] += vmatrix[l*column +k] * other.vmatrix[k* other.column +j];
-                    }
-                }
-            }
+    std::size_t P = other.column;
+    std::vector<T> B = other.vmatrix;
+    std::vector<T> C(row * P);
+
+    std::vector<std::thread> thrds;
+    thrds.reserve(threads);
+    std::size_t n_cols = P / threads;
+    std::cout << n_cols << std::endl;
+    // Launch threads
+    std::size_t start_col = 0;
+
+    std::size_t n_blocks = P / blockSize;
+    std::size_t remains = P % blockSize;
+    if (remains != 0) {
+      remains = blockSize - remains;
+    }
+    else{
+        remains = blockSize;
+    }
+
+    for (std::size_t i = 0; i < threads; i++) {
+       auto end_col = (start_col+n_cols);
+        if(threads-1 == i)
+        {
+                end_col = P;
+        }
+        thrds.emplace_back([&,start_col,end_col] {
+            blocked_column_multi_output_parallel_mmul(B, C, P,start_col,end_col,remains);
         });
+
+       start_col = end_col;
     }
-   for(std::thread &t : usingThreads){
-        t.join();
-    }
+
+    // Wait for all threads to complete
+    for (auto &t : thrds) t.join();
+    thrds.clear();
+
+    matrix<T> mxt(C, row, P);
     return mxt;
 }
--- test/MatrixMultiplication.cpp
+++ test/MatrixMultiplication.cpp
@@ -1,19 +1,29 @@
 #include "../lib/Matrix.h"
 #include <string>
+#include <chrono>
 
 int main(){
-    std::string str0 = "2 -1\n"
-                      "-5 0\n";
-
-    std::string str1 = "-4 -3\n"
-                       "3 -1\n";
-
-    matrix<int> mxt0;
-    matrix<int> mxt1;
-
+  std::string str0 = "";
+    for(long long int i = 1; i <= 1024*1024; i++){
+        str0 += ' ';
+        str0 += std::to_string(i);
+    }
+    matrix<int> mxt0(1024,1024);
+    matrix<int> mxt1(1024,1024);
     mxt0.setMatrix(str0);
-    mxt1.setMatrix(str1);
+    mxt1.setMatrix(str0);
+    mxt0.setOptimalParams();
+    std::cout << "Using threads:"<< std::thread::hardware_concurrency() << '/' << mxt0.getNumberOfThreads() << std::endl;
+    std::cout << "Chunk size:" << mxt0.getBlockSize() << std::endl;
+    std::cout << "Start...\n";
+    auto start_time = std::chrono::high_resolution_clock::now();
 
     auto newMtx = mxt0 * mxt1;
-    newMtx.display();
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+   // newMtx.display();
+    std::cout << "\n";
+    std::cout<<"Benchmark time: "<< static_cast<double>(duration.count())/1000 << " ms\n";
+
 }
